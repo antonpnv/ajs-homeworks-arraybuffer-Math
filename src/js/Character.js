91c46eb4f1e11d0ae3404f8bc5a4465bc99e3cbe
AntonPnv <anton.panov992@gmail.com>
2023-11-27 16:08:34 +0400

    Changed set attack

--- src/js/Character.js
+++ src/js/Character.js
@@ -40,11 +40,7 @@ export class Character {
   }
 
   set attack(value) {
-    if (!this.stoned) {
-      this._attack = value;
-    } else {
-      const stonedAttack = Math.round(value - Math.log2(this.distance) * 5);
-      this._attack = stonedAttack >= 0 ? stonedAttack : 0;
-    }
+    const baseAttack = value >= 0 ? value : 0;
+    this._attack = baseAttack; // Базовая атака без модификаторов
   }
 }
